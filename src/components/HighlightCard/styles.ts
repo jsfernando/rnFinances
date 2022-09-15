8f3c11b659a172d384b91f3de1919799bb6f0b36
jsfernando <jsfernando@gmail.com.br>
2022-09-15 20:26:08 -0300

    cartoes personalizados conforme o type (entradas, saídas e total)

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -2,14 +2,17 @@ import styled, { css } from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons';
 
+// 6.1 criar a interface para tipar o styled component icon pra receber 
+// o type e mudar a cor do ícone
 interface TypeProps {
   type: 'up' | 'down' | 'total';
 }
 export const Container = styled.View<TypeProps>`
-  background-color: ${({ theme }) => theme.colors.shape};
+  /* background-color: ${({ theme }) => theme.colors.shape}; */
 
-  /* background-color: ${({ theme, type }) =>
-  type === 'total' ? theme.colors.secondary :  theme.colors.shape};  */
+  /* 7.1 */
+  background-color: ${({ theme, type }) =>
+  type === 'total' ? theme.colors.secondary :  theme.colors.shape}; 
 
   width: ${RFValue(300)}px;
   border-radius: 5px;
@@ -25,6 +28,7 @@ export const Header = styled.View`
   justify-content: space-between;
 `;
 
+// 8 para o título type
 export const Title = styled.Text<TypeProps>`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(14)}px;
@@ -33,12 +37,15 @@ export const Title = styled.Text<TypeProps>`
   type === 'total' ? theme.colors.shape :  theme.colors.text_dark};
 `;
 
+// 6.2 tipando com <typeProps>
 export const Icon = styled(Feather)<TypeProps>`
   font-size: ${RFValue(40)}px;
 
   color: ${({ theme }) => theme.colors.text_dark};
 
-  /* ${({ type }) => type === 'up' && css`
+  /* 6.3 */
+  /* incluir css no import lin 1 */
+  ${({ type }) => type === 'up' && css`
     color: ${({ theme }) => theme.colors.success};
   `};
 
@@ -48,30 +55,31 @@ export const Icon = styled(Feather)<TypeProps>`
 
   ${({ type }) => type === 'total' && css`
     color: ${({ theme }) => theme.colors.shape};
-  `}; */
+  `};
 `;
 
 export const Footer = styled.View``;
 
+//9
 export const Amount = styled.Text<TypeProps>`
   font-family: ${({ theme }) => theme.fonts.medium};
   font-size: ${RFValue(32)}px;
 
-  color: ${({ theme }) => theme.colors.text_dark};
+  /* color: ${({ theme }) => theme.colors.text_dark}; */
 
-  /* color: ${({ theme, type }) =>
-  type === 'total' ? theme.colors.shape :  theme.colors.text_dark}; */
+  color: ${({ theme, type }) =>
+  type === 'total' ? theme.colors.shape :  theme.colors.text_dark};
 
   margin-top: 38px;
 `;
-
+// 10
 export const LastTransaction = styled.Text<TypeProps>`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(12)}px;
 
-  color: ${({ theme }) => theme.colors.text};
+  /* color: ${({ theme }) => theme.colors.text}; */
 
-  /* color: ${({ theme, type }) =>
-  type === 'total' ? theme.colors.shape :  theme.colors.text}; */
+  color: ${({ theme, type }) =>
+  type === 'total' ? theme.colors.shape :  theme.colors.text};
 `;
 
--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -33,9 +33,26 @@ export function Dashboard(){
                 </UserWrapper>
             </Header>
             <HighlightCards>
-                <HighlightCard type='up' title='Entradas' amount='R$ 17.400,00' lastTransaction='Última entrada dia 13 de abril'/>
-                <HighlightCard type='down' title='Saídas' amount='R$ 1.259,00' lastTransaction='Última saída dia 03 de abril'/>
-                <HighlightCard type='up' title='Total' amount='R$ 17.400,00' lastTransaction='Última entrada dia 13 de abril'/>
+                {/* 3 passando as props do cartão sem o type ainda*/}
+                {/* 4 incluir o type=' e mostrar o intelissence  */}
+                <HighlightCard 
+                    type='up'
+                    title='Entradas' 
+                    amount='R$ 17.400,00' 
+                    lastTransaction='Última entrada dia 13 de abril'
+                />
+                <HighlightCard 
+                    type='down' 
+                    title='Saídas' 
+                    amount='R$ 1.259,00' 
+                    lastTransaction='Última saída dia 03 de abril'
+                />
+                <HighlightCard 
+                    type='total' 
+                    title='Total' 
+                    amount='R$ 16.141,00' 
+                    lastTransaction='Última entrada dia 13 de abril'
+                />
             </HighlightCards>
         </Container>
     )
