fe735fbcf4d679a80bea8214ed3bd2eb47eb7e59
jsfernando <jsfernando@gmail.com>
2022-10-16 10:07:31 -0300

    AsyncStorage.removeitem(), newTransaction, reset() useForm and useNavigation

--- readme.md
+++ readme.md
@@ -1,7 +1,19 @@
 # 1
 # instalando o expo async-storage
-[https://docs.expo.dev/versions/latest/sdk/async-storage/]
+ASYNC STORAGE [https://docs.expo.dev/versions/latest/sdk/async-storage/]
 ## expo install @react-native-async-storage/async-storage
 # 2
 # register/index.tsx
 ## try catch error
+
+# uuid react native
+UUID [https://www.npmjs.com/package/react-native-uuid]
+## npm install react-native-uuid
+
+# reset formulário
+## lin 63 ou useForm
+## lin 117, 118, ...
+
+# proxima fase é fazer a navegação para primeira aba usando o useNavigation
+## import useNavigation e declaração const navigation = useNavigation()
+## ajuste conforme forum por conta da tipagem do useNavigation
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -7,10 +7,17 @@ import {
 } from 'react-native';
 
 
-import { Control, FieldValues, useForm } from 'react-hook-form'
 import * as Yup from 'yup';
 import { yupResolver }  from '@hookform/resolvers/yup';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
+
+import { Control, FieldValues, useForm } from 'react-hook-form'
+import {
+    useNavigation,
+    NavigationProp,
+    ParamListBase,
+  } from "@react-navigation/native";
 
 import {InputForm} from '../../components/Form/InputForm';
 
@@ -57,9 +64,13 @@ export function Register() {
         name: 'Categoria'
     })
 
+    // const navigation = useNavigation();
+    const { navigate }: NavigationProp<ParamListBase> = useNavigation();
+
     const {
         control,
         handleSubmit,
+        reset,
         formState: { errors }
     } = useForm<FormData>({
         resolver: yupResolver(schema)
@@ -86,17 +97,43 @@ export function Register() {
         if (category.key === 'category')
             return Alert.alert('Selecione a categoria')
 
-        const data ={
+        // inserindo a data da transação
+        // 3ª etapa    
+        // id uuid
+        const newTransaction = {
+            id: String(uuid.v4()),
             name: form.name,
             amount: form.amount,
             transactionType,
-            category: category.key
+            category: category.key,
+            date: new Date()
+
         }
 
         // console.log(data)
         try {
-            await AsyncStorage.setItem(dataKey, JSON.stringify(data))
-            // const currentData = data ? 
+            const data = await AsyncStorage.getItem(dataKey);
+            const currentData = data ? JSON.parse(data) : [];
+
+            const dataFormated = [
+                ...currentData,
+                newTransaction
+            ]
+            // 1ª etapa
+            // await AsyncStorage.setItem(dataKey, JSON.stringify(data))
+            // 2ª etapa
+            await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormated))
+
+            // resetando o formulario apos inserido novo lançamento
+            reset();
+            setTransactionType('');
+            setCategory({
+                key: 'category',
+                name: 'Categoria'
+            }) 
+
+            navigate('Listagem');
+
         } catch (error) {
             console.log(error)
             Alert.alert("Não foi possivel salvar")
@@ -110,6 +147,12 @@ export function Register() {
         }
 
         loadData();
+
+        // Removendo a lista atual de transações
+        // async function removeAll(){
+        //     AsyncStorage.removeItem(dataKey);
+        // }
+        // removeAll();
 
     },[])
 
